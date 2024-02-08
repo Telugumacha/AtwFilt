19e46d706bd4cfa43aa49c2d1707a990aab1b084
Percy <percy@railway.app>
2024-02-08 16:32:13 +0000

    Initial Commit

---
+++ plugins/commands.py
@@ -0,0 +1,70 @@
+from random import choice
+from config import START_MSG, FORCE_SUB, BOT_PICS, ADMINS, bot_info, DEV_NAME
+from pyrogram import Client as illuzX, filters as Worker
+from pyrogram.types import InlineKeyboardMarkup,  InlineKeyboardButton, CallbackQuery
+from startup import AtwFilt
+from plugins.database.broadcast_db import Database
+
+db = Database()
+
+
+@illuzX.on_message(Worker.private & Worker.command(["start"]))
+async def start_message(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
+    if len(message.command) != 2:
+        if message.from_user.id not in ADMINS: 
+            buttons = [[
+                 InlineKeyboardButton("Aʙᴏᴜᴛ 🦜" , callback_data= "about"),
+                 InlineKeyboardButton("Sᴇᴀʀᴄʜ Mᴏᴠɪᴇ🔎", switch_inline_query_current_chat='')
+                ], [
+                 InlineKeyboardButton("⚙️ BᴏT Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ⚙️", url="https://t.me/mvbzzer")
+                ], [
+                 InlineKeyboardButton("Hᴏᴡ Tᴏ Usᴇ Mᴇ ❓ ",  url="https://t.me/+2QLvbzUUdB8yNjM1")
+             ]]
+        else:
+            buttons = [[
+                 InlineKeyboardButton("Aʙᴏᴜᴛ 🦜" , callback_data="about"),
+                 InlineKeyboardButton("Sᴇᴀʀᴄʜ Mᴏᴠɪᴇ🔎", switch_inline_query_current_chat='')
+                ], [
+                 InlineKeyboardButton("⚙️ BᴏT Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ⚙️", url="https://t.me/mvbzzer")
+                ], [
+                 InlineKeyboardButton("Hᴏᴡ Tᴏ Usᴇ Mᴇ ❓ " ,  url="https://t.me/+2QLvbzUUdB8yNjM1")
+             ]]    
+        await message.reply_photo(photo = choice(BOT_PICS), caption=AtwFilt.START_TXT.format(mention = message.from_user.mention, bot_name = bot_info.BOT_NAME, bot_username = bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons))
+        
+    elif len(message.command) ==2 and message.command[1] in ["subscribe"]:
+        FORCES=["https://telegra.ph/file/10e57b92fde7aa7b540cf.jpg"]
+        invite_link = await bot.create_chat_invite_link(int(FORCE_SUB))
+        button=[[
+         InlineKeyboardButton("🦋Join To Use Me", url=invite_link.invite_link)
+         ]]
+        reply_markup = InlineKeyboardMarkup(button)
+        await message.reply_photo(
+            photo=choice(FORCES),
+            caption=f"""<i><b>Hello {message.from_user.mention}. \nYou Have <a href="{invite_link.invite_link}">Not Join</a> To <a href="{invite_link.invite_link}">My Update Channel</a>.So you do not get access To Files on Inline Mode, Bot Pm and Group</i></b>""",
+            reply_markup=reply_markup
+        )
+        return
+@illuzX.on_message(Worker.private & Worker.command(["help"]))
+async def help(bot, message):
+    button = [[
+     InlineKeyboardButton("source code", callback_data="SOURCE_CODE"),
+     InlineKeyboardButton("About 😎", callback_data="about")
+     ]]
+    await message.reply_photo(
+        photo = choice(BOT_PICS),
+        caption=AtwFilt.HELP_MSG.format(mention=message.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(button))
+      
+@illuzX.on_message(Worker.private & Worker.command(["about"]))
+async def about(bot, message):
+    button = [[
+     InlineKeyboardButton("🏠 Home", callback_data="start"),
+     InlineKeyboardButton("Close 🗑️", callback_data="close")
+     ]]  
+    await message.reply_photo(
+        photo = choice(BOT_PICS),
+        caption=AtwFilt.ABOUT_MSG.format(mention=message.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME, dev_name=DEV_NAME),
+        reply_markup=InlineKeyboardMarkup(button))
+        
---
+++ startup.py
@@ -0,0 +1,77 @@
+class AtwFilt(object):
+
+    START_TXT = """hy {mention}..
+Iam [{bot_name}](t.me/{bot_username}) 𝙾𝚛 𝚢𝚘𝚞 𝚌𝚊𝚗 𝚌𝚊𝚕𝚕 𝚖𝚎 𝚊𝚜 [𝐅𝐢𝐥𝐭𝐞𝐫 𝐛𝐨𝐭 𝐯𝟐](t.me/{bot_username}) \n
+𝙷𝚎𝚛𝚎 𝚈𝚘𝚞 𝙲𝚊𝚗 𝚁𝚎𝚚𝚞𝚎𝚜𝚝 𝙼𝚘𝚟𝚒𝚎'𝚜, 𝙹𝚞𝚜𝚝 𝚂𝚎𝚗𝚝
+[𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎](t.me/{bot_username})
+𝚆𝚒𝚝𝚑 𝙿𝚛𝚘𝚙𝚎𝚛 #𝙶𝚘𝚘𝚐𝚕𝚎 𝚂𝚙𝚎𝚕𝚕𝚒𝚗𝚐..!!\n
+𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐃𝐞𝐭𝐚𝐢𝐥𝐬 𝐂𝐥𝐢𝐜𝐤 /help"""
+
+    HELP_MSG = """**
+<p><b>Help Module</b></p>
+- For Help Contact [uzxBots](t.me/uzx_bots)\n
+- You want Sorcue Code Then Press On Sorcue Code Button..."""
+    SOURCE_CODE = """
+<p><b>DISCLAIMER</b></p>
+ - **__PLEASE FORK AND USE__**,
+ - **__GIVE A STAR ⭐ TO MY PROJECT__**,
+ - **__CONTRIBUTORS ARE WLCOM__**
+ 
+ <p><b>Repo</b></p>
+[𝐂𝐨𝐝𝐞](https://www.github.com/illuzX/AtwFilt)
+**𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫**
+[𝐈𝐥𝐥𝐮𝐳𝐗](t.me/illuzX3)"""
+
+    ABOUT_MSG = """
+𝐀𝐁𝐎𝐔𝐓 𝐌𝐄
+
+○ 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : [𝐈𝐥𝐥𝐮𝐳𝕏](t.me/illuzX3)
+○ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : [𝐏𝐲𝐭𝐡𝐨𝐧 3.10.6](https://www.python.org/)
+○ 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : [𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 2.0.57](https://docs.pyrogram.org/)
+○ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : [𝐔𝐳𝕏_𝐁𝐨𝐭𝐬](https://t.me/UZX_BOTS)
+○ 𝐃𝐚𝐭𝐚𝐁𝐚𝐬𝐞:[𝐌𝐎𝐍𝐆𝐎𝔻𝔹](https://mongodb.com)
+○ 𝐁𝐮𝐢𝐥𝐝 𝐒𝐭𝐚𝐭𝐮𝐬 : [V2](t.me/meow_pm_bot)
+"""
+#https://github.com/illuzX/AtwFilt/commit/42d151f309bcfc8fe667a9379a7609633705c4e
+
+#FILE : <code>{file_name}</code> 
+#❤️Size : <i>{file_size}</i>
+#✅CAPTION: {file_caption}
+    FILE_CAPTIONS = """ 
+<code>{title}</code> 
+Size : <b>{size}</b>
+━━━━━━━━━━━━━━━━━━━━━━
+<b>[Mᴏᴠɪᴇ Bᴀᴢᴢᴇʀ](https://t.me/mvbzzer)</b
+>\n[Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ Gʀᴏᴜᴘ](https://t.me/mvb_tg)
+**"""
+
+    
+#~~Not work anymore~~
+   # illuzX = """
+#**ADMINS COMMANDS**
+
+ #» /broadcast - Reply Any Media Or Message
+# » /delete - Reply Files
+# » /deleteall - Delete All Files
+ #» /total - How Many Files Saved
+ #» /logger - Get Bot Logs
+ #» /channel - Add Channel List"""
+# Close ????
+
+    ADD_YOUR_GROUP = """**
+𝐼 𝐶𝑎𝑛'𝑡 𝐹𝑖𝑛𝑑  <i>#{query}</i> 𝑖𝑛 𝑚𝑦 𝑑𝑎𝑡𝑎𝑏𝑎𝑠𝑒 𝑠𝑜 𝑝𝑙𝑒𝑎𝑠𝑒 𝑐ℎ𝑒𝑐𝑘 𝑦𝑜𝑢'𝑟𝑒 𝑒𝑛𝑡𝑒𝑟𝑒𝑑 𝑠𝑝𝑒𝑙𝑙𝑖𝑛𝑔 𝑖𝑛 #𝐺𝑜𝑜𝑔𝑙𝑒/ 𝑂𝑟 𝑇ℎ𝑎𝑡 𝑀𝑜𝑣𝑖𝑒 𝑁𝑜𝑡 𝑅𝑒𝑙𝑒𝑎𝑠𝑒𝑑 /**"""
+ 
+    SPELL_CHECK = """
+Hello 👋<spoiler>〘 {mention} 〙, </spoiler>
+Couldn't Find {query}?  Please Click Your Request Movie Name"""
+    GET_MOVIE_1 = """
+** 📁 Here is What I Found In My Database** **For Your Query : #{title}**"""
+
+    GET_MOVIE_2 = """
+📽️ Requested Movie :<b> {query}</b>
+
+👤 Requested By :<b> {mention} </b>
+
+Uploder : <b>{chatb</b>
+"""
+
